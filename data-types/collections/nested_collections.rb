b1c59389a74b56f42b128a98675718917087bcac
Andrew Reid <areid.sj@gmail.com>
2020-01-01 13:12:27 -0700

    Update gemfile

--- data-types/collections/nested_collections.rb
+++ data-types/collections/nested_collections.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class NestesdCollectionsTest < Minitest::Test
 
@@ -13,11 +14,10 @@ class NestesdCollectionsTest < Minitest::Test
   end
 
   def test_2
-    skip
     coordinates = [[2,5],[87,2],[56,39],[3,46]]
     # Using the coordinates variable defined above
     # Retrive the value 39
-    thirty_nine = _________
+    thirty_nine = coordinates[2]
     assert_equal 39, thirty_nine
   end
 
