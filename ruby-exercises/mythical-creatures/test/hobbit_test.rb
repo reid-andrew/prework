fd32563b85a376d4d6cf7a3014d3db70b9ab44b2
Andrew Reid <areid.sj@gmail.com>
2020-01-01 13:45:55 -0700

    Hobbit exercise initial complete

--- ruby-exercises/mythical-creatures/test/hobbit_test.rb
+++ ruby-exercises/mythical-creatures/test/hobbit_test.rb
@@ -2,33 +2,35 @@ gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/hobbit'
+require 'pry'
 
 class HobbitTest < Minitest::Test
   def test_it_has_a_name
+    # skip
     hobbit = Hobbit.new("Bilbo")
     assert_equal "Bilbo", hobbit.name
   end
 
   def test_it_is_named_something_else
-    skip
+    # skip
     hobbit = Hobbit.new("Peregrin")
     assert_equal "Peregrin", hobbit.name
   end
 
   def test_disposition_is_unadventurous
-    skip
+    # skip
     hobbit = Hobbit.new("Samwise")
     assert_equal "homebody", hobbit.disposition
   end
 
   def test_can_have_a_different_disposition
-    skip
+    # skip
     hobbit = Hobbit.new("Frodo", "adventurous")
     assert_equal "adventurous", hobbit.disposition
   end
 
   def test_grows_older_when_celebrating_birthdays
-    skip
+    # skip
     hobbit = Hobbit.new('Meriadoc')
     assert_equal 0, hobbit.age
     5.times do
@@ -38,7 +40,7 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_considered_a_child_at_32
-    skip
+    # skip
     hobbit = Hobbit.new('Gerontius')
     32.times do
       hobbit.celebrate_birthday
@@ -47,7 +49,7 @@ class HobbitTest < Minitest::Test
   end
 
   def test_comes_of_age_at_33
-    skip
+    # skip
     hobbit = Hobbit.new('Otho')
     33.times do
       hobbit.celebrate_birthday
@@ -60,25 +62,42 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_old_at_age_of_101
-    skip
+    # skip
     # create a hobbit
     # have hobbit age 101 years
     # check that hobbit.old? returns true
+    hobbit = Hobbit.new('Henry')
+    101.times do
+      hobbit.celebrate_birthday
+    end
+    assert hobbit.old?
+
+    # check that hobbit.old returns false under 101 years
+    hobbit = Hobbit.new('Porter')
+    100.times do
+      hobbit.celebrate_birthday
+    end
+    refute hobbit.old?
   end
 
   def test_hobbit_has_the_ring_if_its_name_is_frodo
-    skip
+    # skip
     # create a hobbit named Frodo
     # create a second hobbit named Sam
     # check that .has_ring? for Frodo returns true
     # check that .has_ring? for Sam returns false
+    hobbit = Hobbit.new('Frodo')
+    assert hobbit.has_ring?
+    hobbit = Hobbit.new('Sam')
+    refute hobbit.has_ring?
   end
 
   def test_hobbits_are_short
-    skip
+    # skip
     # create a hobbit
     # check that .is_short? returns true
+    hobbit = Hobbit.new('Henry Porter')
+    assert hobbit.is_short?
   end
 
 end
-
